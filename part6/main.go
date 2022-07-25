742b2dde1478a3e49ea27580f8174c6c821b679e
serednitskiy <serednitsky.nick@gmail.com>
2022-07-25 21:34:57 +0300

    Добавил output по Task1 & Task2

--- part6/main.go
+++ part6/main.go
@@ -77,12 +77,12 @@ func main() {
 	//Task1
 
 	array1 := [6]string{"a", "b", "c", "d", "e", "f"}
-	fmt.Println(make([]int, 3, 9))
-	fmt.Println(array1[2:5])
+	fmt.Println(make([]int, 3, 9)) // output: [0 0 0]
+	fmt.Println(array1[2:5])       // output: [c d e]
 
 	//Task2: sort
 
 	array2 := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
 	sort.Ints(array2)
-	fmt.Println(array2[0])
+	fmt.Println(array2[0]) // output: 9
 }
